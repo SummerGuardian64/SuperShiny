52f6b5483ebcf9d57e5532e9c6fae484035fb28c
SummerGuardian64 <summerg64-gh@email1.me>
2025-11-24 08:24:23 +0100

    Profi-profi

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,10 @@
-﻿// main.cpp (game side)
-#include "ssge/Program.h"
-#include "SuperShiny/SuperShiny.h"
+﻿#include "ssge/Program.h" // Super Shiny Game Engine
+#include "SuperShiny/SuperShiny.h" // Super Shiny game
 
 int main(int argc, char** argv)
 {
+	// Create Super Shiny implementation object
 	auto game = SuperShiny::makeGame();
+	// Start program, pass IGame and argc, and return what it returns.
 	return ssge::Program::run(game, argc, argv);
 }
